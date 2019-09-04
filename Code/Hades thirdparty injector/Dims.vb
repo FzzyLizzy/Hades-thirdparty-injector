0c6b00aae86cf583ffcc8477d3b21d1106076f31
Quinn <qschukkink@gmail.com>
2019-09-04 23:25:32 +0200

    remove source code from master

--- Code/Hades thirdparty injector/Dims.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports System.IO
-
-Module Dims
-    Public DllFIle As String = "Hades.dll"
-    Public JVersion As String = "2.5.4"
-    Public Menu_Bin As String
-    Public strPath As String = Directory.GetCurrentDirectory()
-End Module
